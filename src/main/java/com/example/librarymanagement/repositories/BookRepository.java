c31aa2dc4d4c1a428699671a51ce3c9714397d02
ellendi0 <dobish03@gmail.com>
2024-07-18 04:52:21 +0300

    Added service and controller for Book

--- src/main/java/com/example/librarymanagement/repositories/BookRepository.java
+++ src/main/java/com/example/librarymanagement/repositories/BookRepository.java
@@ -3,5 +3,8 @@ package com.example.librarymanagement.repositories;
 import com.example.librarymanagement.models.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findBookByTitleAndAuthor(String title, String author);
 }
