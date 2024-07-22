bac30e40ffbbd78d3e832dbc09d2019c934fe28f
ellendi0 <dobish03@gmail.com>
2024-07-22 12:54:27 +0300

    11: Modified Enums

--- src/main/java/com/example/librarymanagement/model/enums/Availability.java
+++ src/main/java/com/example/librarymanagement/model/enums/Availability.java
@@ -1,5 +1,29 @@
 package com.example.librarymanagement.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Availability {
-    AVAILABLE, NOT_AVAILABLE
+    AVAILABLE("Available"), NOT_AVAILABLE("Unavailable");
+
+    private String value;
+
+    Availability(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static Availability fromString(String value) {
+        for (Availability availability: Availability.values()) {
+            if (availability.value.equalsIgnoreCase(value)) {
+                return availability;
+            }
+        }
+        throw new IllegalArgumentException("No availability with value " + value + ".\nMust be one of available, unavailable");
+    }
 }
--- src/main/java/com/example/librarymanagement/model/enums/Genre.java
+++ src/main/java/com/example/librarymanagement/model/enums/Genre.java
@@ -1,5 +1,31 @@
 package com.example.librarymanagement.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Genre {
-    PROSE, DRAMA, POETRY
+    POETRY("Poetry"),
+    PROSE("Prose"),
+    DRAMA("Drama");
+
+    private String value;
+
+    Genre(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static Genre fromString(String value) {
+        for (Genre genre : Genre.values()) {
+            if (genre.value.equalsIgnoreCase(value)) {
+                return genre;
+            }
+        }
+        throw new IllegalArgumentException("There is no genre with value " + value + ".\nMust be one of Poetry, Prose, Drama");
+    }
 }
