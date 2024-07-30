2bb70b3c1f600c112a4e67143a5235c4fa58bb1d
Olena Dobysh <91687183+ellendi0@users.noreply.github.com>
2024-07-30 15:15:13 +0300

    Refactored publisher dto

--- src/main/java/com/example/librarymanagement/dto/PublisherDto.kt
+++ src/main/java/com/example/librarymanagement/dto/PublisherDto.kt
@@ -3,10 +3,10 @@ package com.example.librarymanagement.dto
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
-class PublisherDto (
+data class PublisherDto (
     val id: Long,
 
     @field:Size(max = 50, message = "Publisher must contain no more than 50 characters")
     @field:NotBlank(message = "Publisher can't be blank")
-    var name: String
+    val name: String
 )
