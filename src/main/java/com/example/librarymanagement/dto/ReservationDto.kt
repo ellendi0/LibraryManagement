9805df6f0c64d54231c232d89d754d477c6b265c
ellendi0 <dobish03@gmail.com>
2024-07-30 15:09:43 +0300

    Refactored reservation dto

--- src/main/java/com/example/librarymanagement/dto/ReservationDto.kt
+++ src/main/java/com/example/librarymanagement/dto/ReservationDto.kt
@@ -1,7 +1,7 @@
 package com.example.librarymanagement.dto
 
 data class ReservationDto(
-    var id: Long,
+    val id: Long,
     val bookTitle: String,
     val author: String,
     val nameOfLibrary: String? = null
