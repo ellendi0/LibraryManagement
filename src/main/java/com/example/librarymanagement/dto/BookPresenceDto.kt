2fba329a49ce38084b7981a705c0777eafa9e7bb
Olena Dobysh <91687183+ellendi0@users.noreply.github.com>
2024-07-30 15:18:15 +0300

    Refactored library dto

--- src/main/java/com/example/librarymanagement/dto/BookPresenceDto.kt
+++ src/main/java/com/example/librarymanagement/dto/BookPresenceDto.kt
@@ -2,7 +2,7 @@ package com.example.librarymanagement.dto
 
 import com.example.librarymanagement.model.enums.Availability
 
-class BookPresenceDto (
+data class BookPresenceDto (
     val id: Long,
     val user: UserResponseDto ?= null,
     val bookTitle: String,
