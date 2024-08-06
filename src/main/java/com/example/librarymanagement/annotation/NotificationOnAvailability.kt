c91c21b5984702688ff9922a5d1df0b02668df40
ellendi0 <dobish03@gmail.com>
2024-08-06 15:38:08 +0300

    Added BeanPostProcessor and modified BookPresence and User service

---
+++ src/main/java/com/example/librarymanagement/annotation/NotificationOnAvailability.kt
@@ -0,0 +1,5 @@
+package com.example.librarymanagement.annotation
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class NotificationOnAvailability
---
+++ src/main/java/com/example/librarymanagement/service/impl/AvailabilityNotificationServiceImpl.kt
@@ -0,0 +1,22 @@
+package com.example.librarymanagement.service.impl
+
+import com.example.librarymanagement.repository.ReservationRepository
+import com.example.librarymanagement.service.AvailabilityNotificationService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class AvailabilityNotificationServiceImpl(
+    private val reservationRepository: ReservationRepository
+): AvailabilityNotificationService {
+
+    private val log: Logger = LoggerFactory.getLogger(AvailabilityNotificationServiceImpl::class.java)
+
+    override fun notifyUserAboutBookAvailability(bookId: Long, libraryId: Long?) {
+        val reservation = reservationRepository.findFirstByBookIdAndLibraryIdOrLibraryIsNull(bookId, libraryId)
+        reservation?.let {
+            log.info("Book with id $bookId is available in library with id $libraryId. " +
+                "User with id ${it.user?.id} can borrow it.")}
+    }
+}
